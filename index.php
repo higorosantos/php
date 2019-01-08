921ca2ead23299bdc55e85b45ca381318fd80309
Higor Oliveira Dos Santos <higorosantos@gmail.com>
2019-01-08 19:10:47 -0200

    Index e action adcionados

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+</head>
+<body>
+    <form method="POST" action="recebe.php">
+    <input type="text" id="texto" name="texto" size=10>
+    <br>
+    <input type="submit" id="sub" name="sub" value="Enviar!!">
+</body>
+</html>
