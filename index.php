2ea103220260542f0b441a1461b783f6a6584873
Higor Oliveira Dos Santos <higorosantos@gmail.com>
2019-01-08 21:36:16 -0200

    ok

--- index.php
+++ index.php
@@ -1,14 +1,34 @@
+<!DOCTYPE <!DOCTYPE html>
 <html>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<head><title>Page Title</title></head>>
 
-<head>
-    <title>Pagina Principal</title>
-</head>
 
 <body>
-    <form method="GET" action="recebe.php">
-        <input type="text" id="texto" name="texto" size="10" required>
-        <br>
-        <input type="submit" id="sub" name="sub" value="Enviar!!">
+<form method="POST" action="recebe.php">
+<form>
+  <div class="form-group">
+  <label for="cpf">CPF</label>
+             <input type="text" class="form-control" id="cpf" name="cpf"placeholder="000.000.000-00" autofocus required>
+    
+  </div>
+  <div class="form-group">
+  <label for="nome">Nome completo</label>
+             <input type="text" class="form-control" id="nome" name="nome" required>
+  </div>
+  <div class="form-group">
+             <label for="email">Email</label>
+             <input type="email" class="form-control" id="email" name="email"placeholder="testando@gmail.com" required>
+             <small id="emailHelp" class="form-text text-muted">Nunca vamos compartilhar seu email, com ninguém.</small>
+         </div>
+  <div class="form-group form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1" name="check">
+    <label class="form-check-label" for="exampleCheck1">Clique em mim</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Confirmar Envio  </button>
+</form>
+ </form>
+    
 </body>
 
-</html>                                                                                                                                                                                                                       
+</html>
--- recebe.php
+++ recebe.php
@@ -1,9 +1,17 @@
 <?php
- echo "O Texto digitado foi: ",$_GET["texto"];
- if($_GET["texto"] == "Higor Oliveira"){
-     echo "<br>Senha Correta";
+ echo "O Nome Digitado foi: ",$_POST["nome"];
+ echo "<br>";
+ echo "<br>";
+ echo "O CPF Digitado foi: ",$_POST["cpf"];
+ echo "<br>";
+ echo "<br>";
+ echo "O Email Digitado foi: ",$_POST["email"]; 
+ echo "<br>";
+ echo "<br>";
+ if (isset($_POST["check"])){
+     echo "Confirmou o otario";
  }
- else{
-    echo "<br>Senha Incorreta";
+ else {
+     echo "O Cara nao Confirmou mano";
  }
 ?>
